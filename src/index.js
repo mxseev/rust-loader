b15eccc070ccc71069496d4edee662648fba86c6
Maxim lis <ralvke@gmail.com>
2017-12-04 16:32:44 +0500

    small refactor

--- src/index.js
+++ src/index.js
@@ -1,77 +1,79 @@
-import {readdirSync, createReadStream, createWriteStream} from "fs"
+import {readdir, createReadStream, createWriteStream} from "fs"
 import {dirname, basename, extname, join, parse} from "path"
 import {spawn} from "child_process"
 import {tmpdir} from "os"
 
-// rustc wrapper
-const rustc = (input, output) => new Promise((resolve, reject) => {
-  const child = spawn("rustc", [
-    "+nightly",
-    "--target=wasm32-unknown-unknown",
-    "--crate-type=cdylib",
-    "-O",
-    input,
-    "-o",
-    output
-  ])
+const spawnWrapper = (name, args) => new Promise((resolve, reject) => {
+  const child = spawn(name, args)
+
+  child.on("error", error => {
+    reject(new Error(`${name} process error: ${error}`))
+  })
+
+  let stderr = ""
+  child.stderr.on("data", data => {
+    if (data) stderr += data
+  })
 
   child.on("close", code => {
     if (code !== 0) {
-      reject(new Error(`rustc ended with non-zero code: ${code}`))
+      reject(new Error(`${name} ended with non-zero code: ${code}\n${stderr}`))
     } else {
       resolve()
     }
   })
 })
 
-// cargo wrapper
-const cargo = (input, output) => new Promise((resolve, reject) => {
-  const child = spawn("cargo", [
+const copy = (from, to) => new Promise((resolve, reject) => {
+  createReadStream(from)
+    .pipe(createWriteStream(to))
+    .on("error", reject)
+    .on("finish", resolve)
+})
+
+const readdirPromise = path => new Promise((resolve, reject) => {
+  readdir(path, {}, (err, files) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(files)
+    }
+  })
+})
+
+const rustc = (input, output) => spawnWrapper("rustc", [
+  "+nightly",
+  "--target=wasm32-unknown-unknown",
+  "--crate-type=cdylib",
+  "-O",
+  input,
+  "-o",
+  output
+])
+
+const cargo = async (input, output) => {
+  await spawnWrapper("cargo", [
     "+nightly",
     "build",
+    `--manifest-path=${input}`,
     "--target=wasm32-unknown-unknown",
-    "--release",
-    `--manifest-path=${input}`
+    "--release"
   ])
 
-  child.on("close", code => {
-    if (code !== 0) {
-      reject(new Error(`Cargo ended with non-zero code: ${code}`))
-    } else {
-      const buildPath = join(
-        dirname(input), "target/wasm32-unknown-unknown/release/"
-      )
+  const buildPath = join(
+    dirname(input), "target/wasm32-unknown-unknown/release"
+  )
+  const buildFiles = await readdirPromise(buildPath)
+  const wasmFiles = buildFiles.filter(file => extname(file) === ".wasm")
+  if (wasmFiles.length === 0) {
+    throw new Error(`no one wasm file in cargo build dir: ${buildPath}`)
+  }
 
-      // TODO: Replace this method with async version
-      const buildFiles = readdirSync(buildPath)
+  const wasmFile = join(buildPath, wasmFiles[0])
+  await copy(wasmFile, output)
+}
 
-      const wasmFiles = buildFiles
-        .filter(filename => extname(filename) === ".wasm")
-
-      if (wasmFiles.length === 0) {
-        reject(new Error(`no one wasm file in cargo build dir: ${buildPath}`))
-      } else {
-        const wasmFile = join(buildPath, wasmFiles[0])
-        createReadStream(wasmFile)
-          .pipe(createWriteStream(output))
-          .on("finish", resolve)
-      }
-    }
-  })
-})
-
-// wasm-gc wrapper
-const wasmGc = filename => new Promise((resolve, reject) => {
-  const child = spawn("wasm-gc", [filename, filename])
-
-  child.on("close", code => {
-    if (code !== 0) {
-      reject(new Error(`wasm-gc ended with non-zero code: ${code}`))
-    } else {
-      resolve()
-    }
-  })
-})
+const wasmGc = filename => spawnWrapper("wasm-gc", [filename, filename])
 
 const glue = file => (`
   module.exports = (() => {
@@ -82,7 +84,6 @@ const glue = file => (`
 
 const rustLoader = async function() {
   const cb = this.async()
-
   const input = this.resourcePath
   const {name, ext} = parse(input)
 
@@ -92,7 +93,6 @@ const rustLoader = async function() {
   } else {
     output = join(tmpdir(), basename(dirname(input)))
   }
-
   output = `${output}.wasm`
 
   try {
@@ -104,7 +104,6 @@ const rustLoader = async function() {
     } else {
       throw new Error(`File type "${ext}" not supported`)
     }
-
     await wasmGc(output)
 
     cb(null, glue(output))
